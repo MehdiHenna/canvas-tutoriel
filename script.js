3a1a306a647e210bfd025e91dc871dc405d79093
MehdiHenna <mehdihennaoui@gmail.com>
2016-01-28 12:02:18 +0100

    Crée un carrée avec canvas

--- script.js
+++ script.js
@@ -1,4 +1,18 @@
 $(document).ready(function(){
 
+	
+	
+	function draw(){
+		var canvas = document.getElementById("canvas");
+		if (canvas.getContext){
+		var ctx = canvas.getContext('2d');
+		
+		ctx.fillRect(25,25,100,100);
+		ctx.clearRect(45,45,60,60);
+		ctx.stokeRect(50,50,50,50);
+		}
+		
+	}
+	draw();
 
 });
