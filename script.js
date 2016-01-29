8176a2de510f46d045a30ab8c095c1638e070450
MehdiHenna <mehdihennaoui@gmail.com>
2016-01-29 09:36:58 +0100

    essaie d'animation

--- script.js
+++ script.js
@@ -1,17 +1,21 @@
 $(document).ready(function(){
 
-	
+	var myInterval = setInterval(animate, 1000/30);
+
 	
 	function draw(){
 		var canvas = document.getElementById("canvas");
 		var ctx= canvas.getContext('2d');
+		
+
 		ctx.beginPath();
 		ctx.arc(100, 75, 50, 0, Math.PI*2);
 		ctx.stroke();
 
-		
-		
 	}
+	
+	
+
 	draw();
 
 });
